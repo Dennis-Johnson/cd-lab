766fa0af10d142193d4e27bc8938d0ec94752580
Dennis Johnson <densarjohn@gmail.com>
2020-12-14 22:24:21 +0530

    Need to fix lab7 parser

---
+++ lab7/new-parser/main.c
@@ -0,0 +1,39 @@
+#include "token.h"
+#include "st.h"
+#include "preprocess.h"
+#include "rdp.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define LEN_BUFFER 128
+
+/*
+Runs a recursive descent parser for the subset of the C grammar from Q7.1. Uses lexer-st from lab4. 
+*/
+
+int main(int argc, char *argv[])
+{
+	if (argc != 2)
+	{
+		fprintf(stderr, "Usage: arg1 -> input c file");
+		exit(EXIT_FAILURE);
+	}
+
+	const char *path;
+
+	// Preprocessing the input c file:
+	path = preprocess(argv[1]);
+
+	fin = fopen(path, "r");
+	if (fin == NULL)
+	{
+		fprintf(stderr, "Could not open processed file %s", path);
+		perror(" ");
+		exit(EXIT_FAILURE);
+	}
+
+  parse(fin);
+	fclose(fin);
+	return 0;
+}
