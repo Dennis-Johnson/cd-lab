ba3ab8a125081e1d701374e2a8ee2ea59a91ad34
Dennis Johnson <densarjohn@gmail.com>
2020-12-24 09:26:35 +0530

    Interface parser still broken

--- interface-grammar/interface/parser.c
+++ interface-grammar/interface/parser.c
@@ -1,13 +1,13 @@
 #include "parser.h"
 #include "lexer.h"
 
-FILE *fin;
 Token *token;
+FILE *fin;
 
 void Intf_Decl();
-void Intf_Modifier();
-void Ex_Intfs();
-void Ex_Intfs_Prime();
+void Intf_Mod();
+void Ex_Intf();
+void Ex_Intf_Prime();
 void Intf_Body();
 void Intf_Mem_Declarations();
 void Intf_Mem_Declarations_Prime();
@@ -15,10 +15,11 @@ void Intf_Mem_Dec();
 void Const_Decl();
 void Const_Idlist();
 void Const_Idlist_Prime();
-void Const_Idlist_PrimePrime();
-void Const_Modifier();
+void Const_Idlist_Prime_Prime();
+void Const_Mod();
 void Type();
 void Intf_Type();
+
 void error(char *str);
 
 void parse()
@@ -31,52 +32,46 @@ void parse()
     //     if (token->type == END_OF_FILE)
     //         break;
     // }
+
     Intf_Decl();
 }
 
 void Intf_Decl()
 {
+    Intf_Mod();
+
     token = getNextToken(fin);
-    if (strcmp(token->name, "public") == 0 || strcmp(token->name, "private") == 0 || strcmp(token->name, "abstract") == 0 || strcmp(token->name, "interface") == 0)
+    if (strcmp(token->name, "interface") == 0)
     {
-        ungetToken(token, fin);
-        Intf_Modifier();
 
         token = getNextToken(fin);
-        if (strcmp(token->name, "interface"))
+        if (token->type == IDENTIFIER)
         {
-
             token = getNextToken(fin);
-            if (token->type == IDENTIFIER)
+            if (strcmp(token->name, "extends") == 0)
             {
-                token = getNextToken(fin);
-                if (strcmp(token->name, "extends") == 0)
-                {
-                    Ex_Intfs();
-                    Intf_Body();
+                Ex_Intf();
+                Intf_Body();
 
-                    token = getNextToken(fin);
-                    if (token->type == END_OF_FILE)
-                    {
-                        printf("Parsing succesful!\n");
-                        return;
-                    }
-                    else
-                        error("END_OF_FILE");
+                token = getNextToken(fin);
+                if (token->type == END_OF_FILE)
+                {
+                    printf("Parsing successful!\n");
                 }
                 else
-                    error("extends");
+                    error("EOF");
             }
             else
-                error("IDENTIFIER");
+                error("extends");
         }
         else
-            error("Interface");
+            error("identifier");
     }
     else
-        error("IntfDecl");
+        error("Interface");
 }
-void Intf_Modifier()
+
+void Intf_Mod()
 {
     token = getNextToken(fin);
     if (strcmp(token->name, "public") == 0 || strcmp(token->name, "private") == 0 || strcmp(token->name, "abstract") == 0)
@@ -84,30 +79,22 @@ void Intf_Modifier()
         return;
     }
     else
-    {
         ungetToken(token, fin);
-    }
 }
 
-void Ex_Intfs()
+void Ex_Intf()
 {
-    token = getNextToken(fin);
-    if (token->type == IDENTIFIER)
-    {
-        Intf_Type();
-        Ex_Intfs_Prime();
-    }
-    else
-        error("IDENTIFIER");
+    Intf_Type();
+    Ex_Intf_Prime();
 }
 
-void Ex_Intfs_Prime()
+void Ex_Intf_Prime()
 {
     token = getNextToken(fin);
     if (token->type == COMMA)
     {
         Intf_Type();
-        Ex_Intfs_Prime();
+        Ex_Intf_Prime();
     }
     else
     {
@@ -121,52 +108,33 @@ void Intf_Body()
     if (token->type == LEFT_CURLY)
     {
         Intf_Mem_Declarations();
+
+        token = getNextToken(fin);
+        if (token->type == RIGHT_CURLY)
+        {
+            return;
+        }
+        else
+            error("Right curly");
     }
     else
-        error("LEFT_CURLY");
+        error("Left curly");
 }
 
 void Intf_Mem_Declarations()
 {
-    token = getNextToken(fin);
-    if (strcmp(token->name, "public") == 0 || strcmp(token->name, "static") == 0 || strcmp(token->name, "final") == 0)
-    {
-        ungetToken(token, fin);
-        Intf_Mem_Dec();
-        Intf_Mem_Declarations_Prime();
-    }
-    else
-    {
-        ungetToken(token, fin);
-    }
+    Intf_Mem_Dec();
+    Intf_Mem_Declarations_Prime();
 }
 void Intf_Mem_Declarations_Prime()
 {
-    token = getNextToken(fin);
-    if (strcmp(token->name, "public") == 0 || strcmp(token->name, "static") == 0 || strcmp(token->name, "final") == 0)
-    {
-        ungetToken(token, fin);
-        Intf_Mem_Dec();
-        Intf_Mem_Declarations_Prime();
-    }
-    else
-    {
-        ungetToken(token, fin);
-    }
+    Intf_Mem_Dec();
+    Intf_Mem_Declarations_Prime();
 }
 
 void Intf_Mem_Dec()
 {
-    token = getNextToken(fin);
-    if (strcmp(token->name, "public") == 0 || strcmp(token->name, "static") == 0 || strcmp(token->name, "final") == 0)
-    {
-        ungetToken(token, fin);
-        Const_Decl();
-    }
-    else
-    {
-        ungetToken(token, fin);
-    }
+    Const_Decl();
 }
 
 void Const_Decl()
@@ -174,8 +142,8 @@ void Const_Decl()
     token = getNextToken(fin);
     if (strcmp(token->name, "public") == 0 || strcmp(token->name, "static") == 0 || strcmp(token->name, "final") == 0)
     {
-        ungetToken(token, fin);
-        Const_Modifier();
+        // ungetToken(token, fin);
+        // Const_Mod();
         Type();
         Const_Idlist();
 
@@ -185,12 +153,10 @@ void Const_Decl()
             Const_Decl();
         }
         else
-            error("SEMICOLON");
+            error("Semicolon");
     }
     else
-    {
         ungetToken(token, fin);
-    }
 }
 
 void Const_Idlist()
@@ -201,7 +167,7 @@ void Const_Idlist()
         Const_Idlist_Prime();
     }
     else
-        error("IDENTIFIER");
+        error("Identifier");
 }
 
 void Const_Idlist_Prime()
@@ -219,25 +185,21 @@ void Const_Idlist_Prime()
             token = getNextToken(fin);
             if (token->type == RIGHT_SQUARE)
             {
-                token = getNextToken(fin);
-                if (token->type == COMMA)
-                {
-                    Const_Idlist_PrimePrime();
-                }
-                else
-                    error("COMMA");
+                Const_Idlist_Prime_Prime();
             }
             else
                 error("RIGHT_SQUARE");
         }
         else
-            error("NUMBER");
+            error("Number");
     }
     else
+    {
         ungetToken(token, fin);
+    }
 }
 
-void Const_Idlist_PrimePrime()
+void Const_Idlist_Prime_Prime()
 {
     token = getNextToken(fin);
     if (token->type == COMMA)
@@ -248,15 +210,13 @@ void Const_Idlist_PrimePrime()
         ungetToken(token, fin);
 }
 
-void Const_Modifier()
+void Const_Mod()
 {
     token = getNextToken(fin);
     if (strcmp(token->name, "public") == 0 || strcmp(token->name, "static") == 0 || strcmp(token->name, "final") == 0)
-    {
         return;
-    }
     else
-        error("Const_Modifier");
+        error("Const Modifier");
 }
 
 void Type()
@@ -278,7 +238,7 @@ void Intf_Type()
         return;
     }
     else
-        error("IDENTIFIER");
+        error("Identifier");
 }
 
 void error(char *str)
